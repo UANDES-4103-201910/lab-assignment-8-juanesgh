df09a75fbc681339968c3b56084c3edd98ff709b
juanesgh <jerodriguez@miuandes.cl>
2019-05-09 22:17:35 -0400

    Keyboard lab

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,39 @@
-// Note: $(() => {}); is equivalent to $(document).ready(function(){})
-$(() => {
-	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
-});
+$(document).ready(function(){
+	var ar = ""
+	$(".keyboard_row .key").click(function(){
+	  /*$("#test").hide();*/
+	  var $this = $(this)
+	  var textinput = $this.html();
+	  textinput = textinput.replace(/\s/g,'');
+	  if (textinput == "Space"){
+		  ar = ar.concat(" ");
+	  }
+	  else{
+		ar = ar.concat(textinput);
+		ar = ar.replace(/Accept/g,'');
+		ar = ar.replace(/Cancel/g,'');
+		ar = ar.replace(/Shift/g,'');
+		ar = ar.replace(/->| Tab/g,'');
+		ar = ar.replace(/Bksp/g,'');
+		ar = ar.replace(/Enter/g,'');
+		ar = ar.replace(/\s/g,'');
+	  }
+	  document.getElementById("carl").value = (ar);
+	});
+	/*
+	$(".key .key-special").click(function(){
+
+	});
+	$().click(function(){
+
+	});
+	$().click(function(){
+
+	});
+	$().click(function(){
+
+	});
+	$().click(function(){
+
+	});     CONTINUAR LLENADO*/
+  });
